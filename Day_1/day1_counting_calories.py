0747c39fc287c0aa25944a8095cff58334b490df
tadekradek <105873923+tadekradek@users.noreply.github.com>
2022-12-01 19:43:05 +0100

    Update day1_counting_calories.py

--- Day_1/day1_counting_calories.py
+++ Day_1/day1_counting_calories.py
@@ -18,3 +18,21 @@ elves[0][1]
 # top 3 elves:
 elves[0][1] + elves[1][1] + elves[2][1]
 
+
+## alternative solution, quite similar but without dictionary
+with open('values.txt') as f:
+    lines = f.readlines()
+
+lines = "".join(lines).split("\n\n")
+
+lines = list(enumerate([sum(list(map(int,i.rstrip('\n').split("\n")))) for i in lines]))
+lines
+
+elves = sorted(lines, key = lambda lines: lines[1], reverse= True)
+
+# top 1 elf:
+elves[0][1]
+
+# top 3 elves:
+elves[0][1] + elves[1][1] + elves[2][1]
+
